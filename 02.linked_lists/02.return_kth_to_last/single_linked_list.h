6208885cd7cfff8dfb1d844554e870fd01be94d2
David Song <david.song0802@gmail.com>
2017-09-05 00:49:14 -0400

    added List to linked-list class

--- 02.linked_lists/02.return_kth_to_last/single_linked_list.h
+++ 02.linked_lists/02.return_kth_to_last/single_linked_list.h
@@ -6,6 +6,7 @@ class Node {
 public:
   // defaults
   Node() {};
+  Node(int);
   ~Node() {};
 
   // fields
@@ -13,20 +14,24 @@ public:
   Node* next = NULL;
   
   // methods
-  void print();
+  void print(bool);
   void read();
   void implode();
 };
 
+Node::Node(int value) {
+  this->value = value;
+}
+
 // Prints the whole list
-void Node::print() {
+void Node::print(bool newline = true) {
   cout << value;
 
   if (next != NULL) {
     cout << " ";
-    next->print();
+    next->print(newline);
   } else {
-    cout << endl;
+    if (newline) cout << endl;
   }
 }
 
@@ -61,3 +66,73 @@ void Node::implode() {
   delete this;
 }
 
+class List {
+public:
+
+  // defaults
+  List();
+  ~List() {};
+
+  // fields
+  int length;
+  Node* head;
+  Node* tail;
+
+  // methods
+  void print();
+  // void read();
+  void implode();
+  void append(int);
+  // void prepend();
+  void connect(List*);
+};
+
+List::List() {
+  this->length = 0;
+  this->head = NULL;
+  this->tail = NULL;
+}
+
+void List::implode() {
+  if (this->head != NULL) {
+    this->head->implode();
+  }
+}
+
+void List::print() {
+  cout << "[";
+  if (this->head != NULL) this->head->print(false);
+  cout << "]" << endl;
+}
+
+// adds a new node at the end of list with the value
+void List::append(int value) {
+  Node* new_node = new Node(value);
+  if (this->length == 0) {
+    this->head = new_node;
+    this->tail = new_node;
+  } else {
+    this->tail->next = new_node;
+    this->tail = new_node;
+  }
+  this->length++;
+}
+
+// connects two lists together through pointers
+// careful! calling implode on other after connecting
+// will cause undefined behavior (shared pointers)
+void List::connect(List* other) {
+  if (other == NULL) return;
+
+  if (this->length == 0) {
+    this->head = other->head;
+  } else {
+    this->tail->next = other->head;
+  }
+
+  if (other->length != 0) {
+    this->tail = other->tail;
+  }
+
+  this->length += other->length;
+}
--- 02.linked_lists/02.return_kth_to_last/single_linked_list_test.cpp
+++ 02.linked_lists/02.return_kth_to_last/single_linked_list_test.cpp
@@ -1,9 +1,30 @@
 #include "single_linked_list.h"
 
 int main() {
-  Node* list = new Node;
-  list->read();
-  list->print();
-  list->implode();
+// Using List in favor of Node
+//  Node* node = new Node;
+//  node->read();
+//  node->print();
+//  node->implode();
+
+  List* list1 = new List;
+  list1->print();
+  list1->append(3);
+  list1->append(5);
+  list1->print();
+
+  List* list2 = new List;
+  list2->append(7);
+  list2->append(9);
+  list2->print();
+
+  list1->connect(list2);
+  list1->print();
+  list2->print();
+
+  list1->implode();
+  delete list1;
+  delete list2;
+
   return 0;
 }
