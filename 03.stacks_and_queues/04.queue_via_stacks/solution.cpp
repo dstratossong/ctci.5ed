d8069418dc095842a05a348a5b62e6519b11b51f
David Song <david.song0802@gmail.com>
2018-03-21 15:28:23 +0000

    added 3.4

--- 03.stacks_and_queues/04.queue_via_stacks/solution.cpp
+++ 03.stacks_and_queues/04.queue_via_stacks/solution.cpp
@@ -1,20 +1,50 @@
 #include <iostream>
 
-#include "stack.h"
+#include "../stack.h"
 
 using namespace std;
 
+template <class T>
+class IdioticQueue {
+  public:
+  LinkedStack<T> in;
+  LinkedStack<T> out;
+
+  void push(T item) {
+    in.push(item);
+  }
+
+  T pop() {
+    if (out.is_empty()) {
+      if (in.is_empty()) throw "empty queue";
+
+      while (! in.is_empty()) {
+        out.push(in.pop());
+      }
+    }
+
+    return out.pop();
+  }
+};
 
 
 int main() {
-  LinkedStack<int> s;
-  s.push(3);
-  s.push(5);
+  IdioticQueue<int> s;
 
-  cout << s.is_empty() << endl;
+  s.push(3);
+  s.push(4);
+
+  cout << s.pop() << endl;
+
+  s.push(5);
+  s.push(6);
+  s.push(7);
+
   cout << s.pop() << endl;
   cout << s.pop() << endl;
-  cout << s.is_empty() << endl;
+  cout << s.pop() << endl;
+  cout << s.pop() << endl;
+  
 
   return 0;
 }
